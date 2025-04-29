24a6ec4324afa747796b2cb7665c6de74eb7cf00
Sudip Ghimire <ghimiresdp@gmail.com>
2025-04-29 15:46:53 +0000

    feat: add tables loans and fines

--- projects/library-management-system/sql/001-create-tables.sql
+++ projects/library-management-system/sql/001-create-tables.sql
@@ -1,3 +1,5 @@
+BEGIN;
+
 --  Create table lib_books
 CREATE TABLE IF NOT EXISTS lib_books (
     id SERIAL PRIMARY KEY,
@@ -8,11 +10,32 @@ CREATE TABLE IF NOT EXISTS lib_books (
     total_copies INTEGER DEFAULT 0,
     available_copies INTEGER DEFAULT 0
 );
+
 -- create table members
-CREATE TABLE IF NOT EXISTS lib_members(
+CREATE TABLE IF NOT EXISTS lib_members (
     id SERIAL PRIMARY KEY,
+    email VARCHAR(128) UNIQUE NOT NULL,
     name VARCHAR(128) NOT NULL,
-    email VARCHAR(128) NOT NULL,
     phone VARCHAR(32),
     join_date DATE DEFAULT CURRENT_DATE
 );
+
+-- create table loan
+CREATE TABLE lib_loans (
+    id SERIAL PRIMARY KEY,
+    book_id INTEGER REFERENCES lib_books (id),
+    member_id INTEGER REFERENCES lib_members (id),
+    issue_date DATE DEFAULT CURRENT_DATE,
+    due_date DATE NOT NULL,
+    return_date DATE NULL -- we do not know it initially
+);
+
+-- create table fines
+CREATE TABLE lib_fines (
+    id serial PRIMARY KEY,
+    loan_id INTEGER REFERENCES lib_loans (id),
+    amount INTEGER,
+    is_paid BOOLEAN DEFAULT FALSE
+);
+
+COMMIT;
